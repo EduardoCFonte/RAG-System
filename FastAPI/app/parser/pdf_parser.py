7e241aec08a3ec98647fb17d0e47a3b6ac8e110b
EduardoCFonte <dudufonte0@gmail.com>
2025-11-11 23:33:33 -0300

    Finalizing chunker and gettint the ml_model

--- FastAPI/app/parser/pdf_parser.py
+++ FastAPI/app/parser/pdf_parser.py
@@ -2,18 +2,28 @@ import fitz
 import sys
 from fastapi import File, UploadFile
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-import nltk
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain_huggingface import HuggingFaceEmbeddings
-
+from langchain_community.vectorstores import Chroma
+from app.core.ml_model import ml_models
+from app.core.load_env import settings
 
 class pdf_parser:
     def __init__(self):
-        print("oi")
+        self.embeddings_model = ml_models.get("embeddings_model")
+        if self.embeddings_model is None:
+            raise RuntimeError("Embedding model not .")
+        self.splitter = RecursiveCharacterTextSplitter(chunk_size=2200,
+                                                  chunk_overlap=50,
+                                                  length_function=len,
+                                                  separators=["Art.","\n\n", "\n", ". ", " ", ""])
+        self.vector_store = Chroma(
+            persist_directory=settings.CHROMA_DB_PATH,
+            embedding_function=self.embeddings_model 
+        )
     
     async def read_pdf(self, files: list[UploadFile] = File(...)):
-        
+
+        all_chunks = []
+
         for file in files:
             try:
                 file_bytes = await file.read()
@@ -31,15 +41,17 @@ class pdf_parser:
             print("trying")
             doc.close()
             complete_pdf = "\n".join(full_pdf)
-            self.chunk_op(complete_pdf)
+            all_chunks.extend(self.chunk_op(complete_pdf))
 
-        return complete_pdf
+        if not all_chunks:
+            print("Nenhum chunk de texto foi gerado.")
+            return
+
+        self.vector_store.add_texts(texts=all_chunks)
+
+        print("Documentos processados e armazenados.")
+        return
     
     def chunk_op(self, complete_pdf: list):
-        splitter = RecursiveCharacterTextSplitter(chunk_size=2200,
-                                                  chunk_overlap=50,
-                                                  length_function=len,
-                                                  separators=["Art.","\n\n", "\n", ". ", " ", ""])
-        
-        chunks = splitter.split_text(complete_pdf)
+        chunks = self.splitter.split_text(complete_pdf)
         return chunks
