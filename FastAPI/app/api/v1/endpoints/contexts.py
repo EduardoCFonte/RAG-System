f592e02fe4a1d10a711e5b5436dbc4fc9a21d349
EduardoCFonte <dudufonte0@gmail.com>
2026-02-24 00:11:00 -0300

    Adjusting wrong responde model leading to errors

--- FastAPI/app/api/v1/endpoints/contexts.py
+++ FastAPI/app/api/v1/endpoints/contexts.py
@@ -49,12 +49,12 @@ async def post_contexts(ctx_id:str, request: Request, db: Session = Depends(get_
         )
     
 
-@router.delete("/contexts/{ctx_id}", response_model=schemas.chatHistory) 
+@router.delete("/contexts/{ctx_id}", response_model=dict) 
 async def delete_context(ctx_id:str, request: Request, db: Session = Depends(get_db)):
     email = await services.get_current_user_email(request)
-    deleteCheck = services.context_delete(db,ctx_id,email)
+    deleteCheck = await services.context_delete(db,ctx_id,email)
 
     if deleteCheck:
-        return {"True is correct" , deleteCheck}
+        return {"True is correct" : deleteCheck}
     
-    return {"True is correct" , False}
+    return {"True is correct" : False}
